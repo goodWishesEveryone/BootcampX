ee4ef7fecc0441ef5a2717ca59c962ebac9ee092
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2022-01-05 17:29:46 -0500

    BootcampX Queries 4

--- 4_queries/name_ofTeachers_thatAssisted.sql
+++ 4_queries/name_ofTeachers_thatAssisted.sql
@@ -3,10 +3,10 @@ Name of all teachers that performed an assistance request during a cohort.
 This query needs to select data for a cohort with a specific name, use 'JUL02' for the cohort's name and order by the instructor's name.
 */
 
-SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-FROM assistance_requests AS ar, students, teachers, cohorts
-WHERE ar.student_id = students.id 
-AND ar.teacher_id = teachers.id
-AND cohorts.id = students.id 
-AND cohorts.name = 'JUL02'
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
 ORDER BY teacher;
