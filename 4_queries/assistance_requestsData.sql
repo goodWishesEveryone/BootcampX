e897a754e2aed38540424f849caa42388f3b6c3e
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2022-01-06 18:58:52 -0500

    Final submission for BootcampX Queries 4

--- 4_queries/assistance_requestsData.sql
+++ 4_queries/assistance_requestsData.sql
@@ -1,7 +1,7 @@
 /*
 Get important data about each assistance request.
---This is a test.
-Select the teacher's name, student's name, assignment's name, and the duration of each assistance request.  Subtract completed_at by started_at to find the duration.
+Select the teacher's name, student's name, assignment's name, and the duration of each assistance request.  
+Subtract completed_at by started_at to find the duration.
 Order by the duration of the request.
 */
 
