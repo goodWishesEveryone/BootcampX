ca17dd663cadb3429984ad1c7d6049dd527d2dad
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2022-01-06 23:39:44 -0500

    Add files to use node-postgres library to connect to any PostgreSQL db from any node app.

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,30 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "sm",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+pool
+  .query(
+    `
+    SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+    FROM teachers
+    JOIN assistance_requests ON teacher_id = teachers.id
+    JOIN students ON student_id = students.id
+    JOIN cohorts ON cohort_id = cohorts.id
+    WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
+    GROUP BY teachers.name, cohorts.name
+    ORDER BY teacher;
+`
+  )
+  .then((res) => {
+    res.rows.forEach((user) => {
+      console.log(
+        `${user.cohort}: ${user.teacher}`
+      );
+    });
+  })
+  .catch((err) => console.error("query error", err.stack));
