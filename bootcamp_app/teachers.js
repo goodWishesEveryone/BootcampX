3802c934a8aca3cf5ba88ca61a05097fc47b92cc
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2022-01-09 22:38:11 -0500

    Add LIMIT to Parameterized Query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -10,7 +10,7 @@ const pool = new Pool({
 const cohortName = process.argv[2] || "JUL02";
 const limit = process.argv[3] || 5;
 // Store all potentially malicious values in an array.
-const values = [`%${cohortName}%`];
+const values = [`%${cohortName}%`, limit];
 
 const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -19,7 +19,8 @@ JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name LIKE $1
-ORDER BY teacher;
+ORDER BY teacher
+LIMIT $2;
 `;
 
 pool.query(queryString,values)
